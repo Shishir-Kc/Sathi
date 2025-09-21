68155b883962ba777ffdce06c57540e4a2aaca00
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-21 17:04:18 +0545

    added api key and cross-origin middleware

--- Sathi_Backend/settings.py
+++ Sathi_Backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Validation'
+    'Validation',
+    'corsheaders',
 ]
 
 
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Sathi_Backend.urls'
@@ -128,3 +130,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+"""
+    Groq APi
+
+"""
+
+GROQ_API = Config('GROQ_API')
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:9001",
+    "*",
+]
+CORS_ALLOW_ALL_ORIGINS = True
