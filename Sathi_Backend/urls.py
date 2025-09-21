0e082dea02cafbce7a95bb3a6d9de389a8adb798
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-21 17:04:50 +0545

    updated Api

--- Sathi_Backend/urls.py
+++ Sathi_Backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/Validation/',include('Validation.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/chat/V1/',include('Chat.urls')),
 ]
