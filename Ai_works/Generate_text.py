69de06fdc63958c7ac0ddc1ab1de3cd2d9d3cd1f
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-21 17:05:22 +0545

    logic for sending and receving TTT from Groq

---
+++ Ai_works/Generate_text.py
@@ -0,0 +1,35 @@
+from groq import Groq
+from decouple import config
+import asyncio
+
+client = Groq(api_key=config('GROQ_API'))
+async def Generate_text(prompt) -> str:
+    response = " "
+    completion = client.chat.completions.create(
+    model="openai/gpt-oss-120b",
+    messages=[
+      {
+        "role": "user",
+        "content": prompt
+      }
+    ],
+    temperature=1,
+    max_completion_tokens=8192,
+    top_p=1,
+    reasoning_effort="medium",
+    stream=True,
+    stop=None
+    )
+
+    for chunk in completion:
+     data = chunk.choices[0].delta.content 
+     if data:
+        response += data
+    return response
+
+
+if __name__ == "__main__":
+   prompt = input('=> ')
+   response = asyncio.run(Generate_text(prompt))
+   print("==============================================")
+   print(response)
