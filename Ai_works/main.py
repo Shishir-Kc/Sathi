a8be9664bf9f66c641ddbf00d126d68117b52db1
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-21 17:03:27 +0545

    added Groq Ai with simple text to text conversation

--- Ai_works/main.py
+++ Ai_works/main.py
@@ -2,17 +2,47 @@
 
     This is  a Fast API Server Not A Django Server ! 
 
+    
+    Ai related Task will be done here ! 
+
+"""
+from click import prompt
+from fastapi import FastAPI
+from Generate_text import Generate_text
+from pydantic import BaseModel
+"""
+    preparing for expected json data
+
+"""
+class Chat(BaseModel):
+    prompt:str
+
+app = FastAPI()
+
+
+
+
+"""
+    General API testing ! 
 
 """
 
 
 
-from fastapi import FastAPI
-
-app = FastAPI()
-
 @app.get("/hello")
 async def hello():
     return {
         'message':'helllo this is from Fast API'
+    }
+
+"""
+
+    APi Routing for Ai Response ! 
+
+"""
+@app.post('/chat/')
+async def Chat(data: Chat):
+    response = await Generate_text(data.prompt)
+    return {
+        'response':response
     }
