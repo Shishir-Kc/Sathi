e898f7f79e8844f6071291b4a999876a2e04fa49
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-22 19:03:00 +0545

    added function based view and in urls.py changed defination

--- Chat/views.py
+++ Chat/views.py
@@ -1,25 +1,23 @@
-from rest_framework.decorators import api_view,APIView
+from rest_framework.decorators import api_view,APIView,permission_classes
 from rest_framework.response import Response
-import requests
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+import httpx
+import asyncio
 
 
-class Chat(APIView):
-
-
-    async def post(self,request):
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def Chat(request):
+        
         prompt = request.data.get('prompt')
-        payload = {
-      "prompt": prompt    
-        }
+        payload = {"prompt": prompt}
+        async def response():
+         async with httpx.AsyncClient() as client:
+            response = await client.post('http://127.0.0.1:9000/chat/', json=payload)
+            return response.json()
+        data = asyncio.run(response())    
+        
 
-        response = requests.request('POST','http://127.0.0.1:9000/chat/',json=payload)
-        data = response.json()
-        return Response(
-            {
-                'Message':data.get('response')
-            },
-            status= status.HTTP_200_OK
-        )
-
+        return Response({'Message': data.get('response')}, status=status.HTTP_200_OK)
 
