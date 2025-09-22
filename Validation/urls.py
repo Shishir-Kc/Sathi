8e84e21d3f5454a0a034c4f822edbb01a92c1d57
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-22 19:05:33 +0545

    added url routing for JWT Auth

--- Validation/urls.py
+++ Validation/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import users,get_content
-
-
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 app_name = "Validation"
 
 urlpatterns = [
     path('users/',users,name="users"),
     path('connect/',get_content,name='FastAPi'),
-
+    path('token/',TokenObtainPairView.as_view(),name='Token_obtain_view'),
+    path('token/refresh/',TokenRefreshView.as_view(),name='Token_Refresh_View'),
+    
 ]
