eb5546de3eeb40b231aaacb5cd64d9dd1651562c
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-26 16:40:23 +0545

    using Langchan for text generation

--- Ai_works/Generate_text.py
+++ Ai_works/Generate_text.py
@@ -1,35 +1,52 @@
-from groq import Groq
-from decouple import config
-import asyncio
+from langchain.chat_models import init_chat_model
+import os 
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from tools import search_the_web
 
-client = Groq(api_key=config('GROQ_API'))
-async def generate_text(prompt) -> str:
-    response = " "
-    completion = client.chat.completions.create(
-    model="openai/gpt-oss-120b",
-    messages=[
-      {
-        "role": "user",
-        "content": prompt
-      }
-    ],
-    temperature=1,
-    max_completion_tokens=8192,
-    top_p=1,
-    reasoning_effort="medium",
-    stream=True,
-    stop=None
-    )
+load_dotenv()
 
-    for chunk in completion:
-     data = chunk.choices[0].delta.content 
-     if data:
-        response += data
-    return response
+os.environ["LANGSMITH_API_KEY"]
+os.environ["LANGSMITH_WORKSPACE_ID"]
+os.environ["GROQ_API_KEY"] 
+
+model = init_chat_model("openai/gpt-oss-120b",model_provider="groq")
+
+prompt_template = ChatPromptTemplate([
+  ('system','You are an ai named SKY made by Kc.tech '),
+  MessagesPlaceholder("msg")
+])
 
 
-if __name__ == "__main__":
-   prompt = input('=> ')
-   response = asyncio.run(generate_text(prompt))
-   print("==============================================")
-   print(response)
+
+async def generate_text(prompt):
+  up_prompt = prompt_template.invoke({
+    "msg":[HumanMessage(content=prompt)]
+  })
+  # user_message  = model.invoke(up_prompt)
+  
+  tools =[search_the_web]
+  model_with_tools = model.bind_tools(tools)
+  initial_response = model_with_tools.invoke(up_prompt)
+  if initial_response.tool_calls:
+    tool_call = initial_response.tool_calls[0]
+    tool_args = tool_call['args']
+    tool_name = tool_call['name']
+
+    tool_func = {"search_the_web": search_the_web}[tool_name.lower()]
+    tool_result = tool_func.invoke(tool_args)
+
+    followup_message = [
+        HumanMessage(content=prompt),
+        HumanMessage(content=f"Tool result: {tool_result}")
+    ]
+  
+    final_response = model_with_tools.invoke(followup_message)
+    return final_response.content
+  else:
+    return initial_response.content
+  
+  
+
+
