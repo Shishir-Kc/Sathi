bad2089b275a3e4a60f31165a708966793ae0b2a
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-26 16:42:05 +0545

    idk

--- Ai_works/Sample_model.py
+++ Ai_works/Sample_model.py
@@ -3,13 +3,16 @@ import os
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain.chat_models import init_chat_model
-from langchain_core.tools import tool
+from dotenv import load_dotenv
+load_dotenv()
 
+os.environ["LANGSMITH_API_KEY"]
+os.environ["LANGSMITH_WORKSPACE_ID"]
 
 
 if not os.environ.get("GROQ_API_KEY"):
   os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for Groq: ")
-llm = init_chat_model("openai/gpt-oss-20b", model_provider="groq")
+llm = init_chat_model("openai/gpt-oss-120b", model_provider="groq")
 
 
 @tool
